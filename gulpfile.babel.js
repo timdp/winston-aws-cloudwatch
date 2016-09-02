ebdfe875eac8b1616b79a63aef70e1d9b2b27f72
Tim De Pauw <timdp@users.noreply.github.com>
2016-09-02 08:59:20 +0200

    Remove gulp-notify

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -10,15 +10,10 @@ const COVERAGE_THRESHOLDS = {global: 100}
 const $ = loadPlugins()
 const argv = yargs.string('grep').argv
 
-const plumb = () => $.if(!process.env.CI, $.plumber({
-  errorHandler: $.notify.onError('<%= error.message %>')
-}))
-
 gulp.task('clean', () => del('lib'))
 
 gulp.task('transpile', () => {
   return gulp.src('src/**/*.js')
-    .pipe(plumb())
     .pipe($.sourcemaps.init())
     .pipe($.babel())
     .pipe($.sourcemaps.write())
@@ -27,7 +22,6 @@ gulp.task('transpile', () => {
 
 gulp.task('lint', () => {
   return gulp.src('{src,test}/**/*.js')
-    .pipe(plumb())
     .pipe($.standard())
     .pipe($.standard.reporter('default', {breakOnError: false}))
 })
@@ -40,7 +34,6 @@ gulp.task('pre-coverage', () => {
 
 gulp.task('coverage', ['pre-coverage'], () => {
   return gulp.src(['test/lib/setup.js', 'test/{unit,integration}/**/*.js', '!**/_*.js'], {read: false})
-    .pipe(plumb())
     .pipe($.mocha({
       reporter: 'spec',
       grep: argv.grep
